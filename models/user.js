388577336905918e4b4d2bc9525cb800c4349e7d
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2019-06-13 12:59:22 +0530

    refactor(user): Move logic for auto sending (and generating) password to user Model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt-nodejs")
+const randomstring = require("randomstring")
 
 // const config = require('../config')[process.env.NODE_ENV || 'development'];
+const mailer = require("../lib/mail")
 
 const UserSchema = new mongoose.Schema({
 
@@ -52,6 +54,17 @@ const UserSchema = new mongoose.Schema({
 
 })
 
+
+UserSchema.pre("validate", function (next) {
+  if (this.isNew) {
+    if (this.password === undefined || this.password === null) {
+      this.generatedPassword = randomstring.generate(8) // for usage in post save hook to send welcome email
+      this.password = this.generatedPassword
+    }
+  }
+  return next()
+})
+
 // Save user's hashed password
 UserSchema.pre("save", function (next) {
   const user = this
@@ -88,6 +101,17 @@ UserSchema.methods.comparePassword = function (pw) {
     })
   })
 }
+// eslint-disable-next-line prefer-arrow-callback
+UserSchema.post("save", function (doc) {
+  if (doc.generatedPassword !== undefined) {
+    // Send welcome email, but NO WAITING!
+    mailer("welcome", {
+      to: doc.email,
+      subject: "Welcome!!!",
+      locals: { email: doc.email, password: doc.generatedPassword, name: doc.name }
+    })
+  }
+})
 
 UserSchema.virtual("name.full").get(function () {
   const first = (this.name.first === undefined || this.name.first === null)
--- routes/rest/auth/signup.js
+++ routes/rest/auth/signup.js
@@ -9,26 +9,21 @@ module.exports = {
       const {
         email,
         phone,
-        name
+        name,
+        password
       } = req.body
       if (email === undefined) return res.status(400).json({ error: true, reason: "Missing manadatory field `email`" })
       if (name === undefined || name.first === undefined) return res.status(400).json({ error: true, reason: "Please specify First Name!" })
-      const password = (req.body.password !== undefined)
-        ? req.body.password
-        : randomstring.generate(8) // auto-generated plaintext pass
-      const user = await User.create({
+      let user = await User.create({
         email,
         phone,
         password,
         name
       })
+      user = user.toObject()
       delete user.password
-      // Send welcome email, but NO WAITING!
-      mailer("welcome", {
-        to: email,
-        subject: "Welcome!!!",
-        locals: { email, password, name }
-      })
+      delete user.forgotpassword
+
       return res.json({ error: false, user })
     } catch (err) {
       return res.status(500).json({ error: true, reason: err.message })
