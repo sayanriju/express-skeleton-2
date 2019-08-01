2c8a5fc588d484456edb3a032585beae2c1542fa
Sayan Riju Chakrabarti <s26c.sayan@gmail.com>
2019-08-01 18:36:17 +0530

    docs: Documented api-s and added npm script to generate apidoc

--- routes/rest/auth/password.js
+++ routes/rest/auth/password.js
@@ -6,18 +6,55 @@ const mail = require("../../../lib/mail")
 const User = require("../../../models/user")
 
 module.exports = {
+  /**
+   *
+   * @api {post} /forgotpassword Request to get password reset link in mail
+   * @apiName forgotPassword
+   * @apiGroup Auth
+   * @apiVersion  1.0.0
+   * @apiPermission Public
+   *
+   *
+   * @apiParam  {String} handle (email)
+   *
+   * @apiSuccess (200) {json} name description
+   *
+   * @apiParamExample  {json} Request-Example:
+   * {
+   *     "handle" : "myEmail@logic-square.com"
+   * }
+   *
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * {
+   *     "error" : false,
+   *     "handle" : "myEmail@logic-square.com"
+   * }
+   *
+   *
+   */
   async startWorkflow(req, res) {
     try {
       const { handle } = req.body
       const user = await User.findOne({ email: handle }).exec()
-      if (user === null) throw new Error("User not found, please check your email address and try again")
-      if (!user.isActive) throw new Error("Oops, there is a problem with your account. Please contact admin")
+      if (user === null) {
+        throw new Error(
+          "User not found, please check your email address and try again"
+        )
+      }
+      if (!user.isActive) {
+        throw new Error(
+          "Oops, there is a problem with your account. Please contact admin"
+        )
+      }
       const now = Date.now()
       const token = cuid.slug()
       user.forgotpassword = {
         requestedAt: now,
         token,
-        expiresAt: moment(now).add(65, "minutes").toDate()
+        expiresAt: moment(now)
+          .add(65, "minutes")
+          .toDate()
       }
       await user.save()
       try {
@@ -32,7 +69,9 @@ module.exports = {
         })
       } catch (mailErr) {
         console.log("==> Mail sending Error: ", mailErr)
-        throw new Error("Failed to send Password Reset Email! Please Retry Later.")
+        throw new Error(
+          "Failed to send Password Reset Email! Please Retry Later."
+        )
       }
       return res.status(200).json({ error: false, handle })
     } catch (error) {
@@ -40,16 +79,53 @@ module.exports = {
     }
   },
 
+  /**
+   *
+   * @api {post} /resetpassword Request to set a new password
+   * @apiName resetPassword
+   * @apiGroup Auth
+   * @apiVersion  1.0.0
+   * @apiPermission Public
+   *
+   *
+   * @apiParam  {String} token
+   * @apiParam  {String} password
+   * @apiParam  {String} email
+   *
+   * @apiSuccess (200) {json} name description
+   *
+   * @apiParamExample  {json} Request-Example:
+   * {
+   *     "email" : "myEmail@logic-square.com"
+   * }
+   *
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * {
+   *     "error" : false,
+   *     "email" : "myEmail@logic-square.com"
+   * }
+   *
+   *
+   */
   async resetPassword(req, res) {
     try {
       const { token, password, email } = req.body
       // if (/(?=.*([\d]|[\@\!\#\$\%\^\&\*\-\_\+\\\.\,\;\=])).{8,}/.test(password) === false) { // eslint-disable-line no-useless-escape
       //   throw new Error("Password should have length of at least 8 with one special character or number!")
       // }
-      const user = await User.findOne({ "forgotpassword.token": token, "forgotpassword.expiresAt": { $gte: new Date() }, email }).exec()
+      const user = await User.findOne({
+        "forgotpassword.token": token,
+        "forgotpassword.expiresAt": { $gte: new Date() },
+        email
+      }).exec()
       if (user === null) throw new Error("Invalid or Expired Token")
       // if (user === null) throw new Error("User not found, please check your email address and try again")
-      if (!user.isActive) throw new Error("Oops, there is a problem with your account. Please contact admin")
+      if (!user.isActive) {
+        throw new Error(
+          "Oops, there is a problem with your account. Please contact admin"
+        )
+      }
       user.password = password
       user.lastModifiedAt = Date.now()
       user.forgotpassword = {
--- routes/rest/auth/signup.js
+++ routes/rest/auth/signup.js
@@ -1,16 +1,63 @@
 const User = require("../../../models/user")
 
 module.exports = {
+  /**
+   *
+   * @api {post} /signup User registration
+   * @apiName userRegistration
+   * @apiGroup Auth
+   * @apiVersion  1.0.0
+   * @apiPermission Public
+   *
+   *
+   * @apiParam  {String} email
+   * @apiParam  {String} phone
+   * @apiParam  {Object} name
+   * @apiParam  {String} password
+   *
+   * @apiSuccess (200) {json} name description
+   *
+   * @apiParamExample  {json} Request-Example:
+   * {
+   *     "email" : "myEmail@logic-square.com",
+   *     "phone" : "00000000000",
+   *     "name"  :{
+   *          "first":"Jhon",
+   *          "last" :"Doe"
+   *      }
+   * }
+   *
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * {
+   *     "error" : false,
+   *     "user" : {
+   *          "email" : "myEmail@logic-square.com",
+   *          "phone" : "00000000000",
+   *          "name"  :{
+   *              "first":"Jhon",
+   *              "last" :"Doe"
+   *           }
+   *      }
+   * }
+   *
+   *
+   */
   async post(req, res) {
     try {
       const {
-        email,
-        phone,
-        name,
-        password
+        email, phone, name, password
       } = req.body
-      if (email === undefined) return res.status(400).json({ error: true, reason: "Missing manadatory field `email`" })
-      if (name === undefined || name.first === undefined) return res.status(400).json({ error: true, reason: "Please specify First Name!" })
+      if (email === undefined) {
+        return res
+          .status(400)
+          .json({ error: true, reason: "Missing manadatory field `email`" })
+      }
+      if (name === undefined || name.first === undefined) {
+        return res
+          .status(400)
+          .json({ error: true, reason: "Please specify First Name!" })
+      }
       let user = await User.create({
         email,
         phone,
