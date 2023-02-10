506f901680599ab37c4786cd7e46895447abb127
Sayan "Riju" Chakrabarti <s26c.sayan@gmail.com>
2023-02-10 20:26:34 +0530

    test(unit testing): added full fixture loading for mongo

--- tests/UNIT/routes/rest/users.find.test.js
+++ tests/UNIT/routes/rest/users.find.test.js
@@ -3,42 +3,26 @@ const test = require("ava")
 const faker = require("faker")
 const sinon = require("sinon")
 
-const { runRouteHandler, beforeHookMongo, afterHookMongo } = require("../../../_utils")
+const {
+  runRouteHandler, setupMongo, teardownMongo, setupFixtures, teardownFixtures
+} = require("../../../_utils")
 const User = require("../../../../models/user")
 const { find } = require("../../../../routes/rest/users") // function to unit test
 
-test.before(beforeHookMongo)
-test.after.always(afterHookMongo)
-
-test.beforeEach(async (t) => {
-  // fixture data set:
-  faker.seed(123)
-  const fixture = Array(5).fill().map((x) => ({
-    email: faker.internet.email(),
-    name: faker.name.findName(),
-    password: faker.internet.password
-  }))
-  t.context.fixture = fixture
-  await User.create(fixture)
-})
-test.afterEach(async (t) => {
-  await User.remove({})
-})
+/** Setup & Teardown code (COMMON) */
+test.before(setupMongo)
+test.after.always(teardownMongo)
+test.beforeEach(setupFixtures)
+test.afterEach(teardownFixtures)
+/* ******************************* */
 
 test.serial("Users.find: my passing test", async (t) => {
   const { status, body } = await runRouteHandler(find)
   t.is(status, 200)
-  t.is(body.users.length, 5)
-})
-
-test.serial("Users.find: my second passing test", async (t) => {
-  const { status, body } = await runRouteHandler(find)
-  t.is(status, 200)
-  const givenEmails = t.context.fixture.map(({ email }) => email.toLowerCase())
-  const returnedEmails = body.users.map(({ email }) => email.toLowerCase())
-  const usersMatch = givenEmails.length === returnedEmails.length
-    && returnedEmails.every((email) => givenEmails.includes(email))
-  t.true(usersMatch)
+  t.false(body.error)
+  t.is(body.users.length, 1)
+  t.is(body.users[0].email, "foo@bar.com")
+  t.is(body.users[0].name.full, "Foo Bar")
 })
 
 test.serial("Users.find: If DB ops throw an error, status should be 500", async (t) => {
--- tests/_utils.js
+++ tests/_utils.js
@@ -1,19 +1,38 @@
 const mongoose = require("mongoose")
 const { MongoMemoryServer } = require("mongodb-memory-server")
 const httpMocks = require("node-mocks-http")
+const Fixtures = require("node-mongodb-fixtures")
 
 let mongod
 mongoose.set("strictQuery", true)
 
 module.exports = {
-  async beforeHookMongo(t) {
+  async setupMongo(t) {
     mongod = await MongoMemoryServer.create()
-    await mongoose.connect(await mongod.getUri())
+    // eslint-disable-next-line no-param-reassign
+    t.context.mongoUri = await mongod.getUri()
+    await mongoose.connect(t.context.mongoUri)
   },
-  async afterHookMongo(t) {
+  async teardownMongo(t) {
     await mongoose.disconnect()
     await mongod.stop()
   },
+
+  async setupFixtures(t) {
+    // eslint-disable-next-line no-param-reassign
+    t.context.fixtures = new Fixtures({
+      dir: "tests/_fixtures",
+      mute: true
+    })
+    await t.context.fixtures.connect(t.context.mongoUri)
+    await t.context.fixtures.unload()
+    await t.context.fixtures.load()
+  },
+  async teardownFixtures(t) {
+    await t.context.fixtures.unload()
+    await t.context.fixtures.disconnect()
+  },
+
   async runRouteHandler(fn, req = {}) {
     const request = httpMocks.createRequest({
       method: "GET", // default
